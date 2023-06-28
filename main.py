a424cdc749dd57fbccc8d1fbf4a342b151d5bc06
A.Klivtsov <69029551+HappyAcidRain@users.noreply.github.com>
2023-06-28 12:35:49 +0300

    need to resolve how paint days corectly

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import sys
 from PyQt6 import QtWidgets, QtCore, QtGui
 from PyQt6.QtWidgets import QDialog, QApplication, QTableWidgetItem, QColorDialog
-from PyQt6.QtCore import QPropertyAnimation, QEasingCurve, QPoint, QTimer, QThread, QObject
+from PyQt6.QtCore import QPropertyAnimation, QEasingCurve, QPoint, QTimer, QThread, QObject, Qt
+from PyQt6.QtGui import QColor
 
 # окна
 import mainUI
@@ -12,18 +13,21 @@ import editUI
 import sqlite3
 
 # основное окно
-class MainWindow(QtWidgets.QMainWindow, mainUI.Ui_MainWindow, QDialog):
+class MainWindow(QtWidgets.QMainWindow, mainUI.Ui_MainWindow, QDialog, QColor):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
-        self.settingTable()
         self.setWindowTitle("BusinessThing")
+        self.settingTable()
 
         self.tw_table.cellClicked.connect(self.cellWasClicked)
         self.tw_table.cellDoubleClicked.connect(self.clearCell)
 
         self.btn_notes.clicked.connect(self.edit)
+
+        self.editWindow = EditWindow()
+        self.editWindow.mainInfo.connect(self.converter)
 
     def settingTable(self):
 
@@ -52,21 +56,69 @@ class MainWindow(QtWidgets.QMainWindow, mainUI.Ui_MainWindow, QDialog):
         self.tw_table.setItem(row, column, QTableWidgetItem())
 
     def edit(self):
-        self.e = EditWindow()
-        self.e.show()
+        self.editWindow.show()
+
+    def update(self, satate):
+        print(state)
+
+    def converter(self, monthIn, dayIn, monthOut, dayOut):
+
+        print(monthIn)
+        print(dayIn)
+        print(monthOut)
+        print(dayOut)
+
+        x = dayOut-dayIn
+
+        day = 0
+
+        while day <= dayOut:
+
+
+        print(x)
+
+        for day in range(x):
+            self.tw_table.setItem(0, day, QTableWidgetItem())
+            self.tw_table.item(0, day).setBackground(QtGui.QColor(100,100,150))
 
 
 # окно изменения
 class EditWindow(QtWidgets.QMainWindow, editUI.Ui_MainWindow, QDialog):
+    mainInfo = QtCore.pyqtSignal(int, int, int, int)
+    otherInfo = QtCore.pyqtSignal(str)
+
     def __init__(self):
         super(EditWindow, self).__init__()
         self.setupUi(self)
 
-        self.btn_color.clicked.connect(self.color)
+        self.btn_color.clicked.connect(self.colorDialog)
+        self.btn_save.clicked.connect(self.save)
+        
+        self.color = None
+        self.monthIn = None
+        self.monthOut = None
+        self.dayIn = None
+        self.dayOut = None
+        self.timeIn = None
+        self.timeOut = None
+        self.notes = None
 
-    def color(self):
-        color = QColorDialog.getColor()
-        print(color)
+    def colorDialog(self):
+        self.color = QColorDialog.getColor()
+
+    def save(self):
+        self.monthIn = self.date_in.date().month()
+        self.monthOut = self.date_out.date().month()
+        self.dayIn = self.date_in.date().day()
+        self.dayOut = self.date_out.date().day()
+
+        self.mainInfo.emit(self.monthIn, self.dayIn, self.monthOut, self.dayOut)
+
+        self.timeIn = self.time_in.time().toString(Qt.DateFormat.ISODate)
+        self.timeOut = self.time_out.time().toString(Qt.DateFormat.ISODate)
+
+        self.notes = self.te_notes.toPlainText()
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
