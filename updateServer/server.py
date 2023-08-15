59010c398ae545335f54cff3b888c998cb82a831
A.Klivtsov <Sashaklivsovpechka@gmail.com>
2023-08-15 14:47:34 +0300

    server structure changed

--- updateServer/server.py
+++ updateServer/server.py
@@ -1,33 +1,42 @@
 import socket
+import os
 
-IP = '31.131.68.162'
-PORT = 8080
-ADDR = (IP, PORT)
+# БД
+import sqlite3
 
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-    server.listen()
 
-    print("[STARTING] Server has been started and now listening.")
-    print("[INFO] Server's addr is: " + str(IP) + ":"+ str(PORT))
-    print("[INFO] You can change ip and port by editing server.py file")
+class Server():
+    def __init__(self):
+
+        self.IP = '31.131.68.162'
+        self.PORT = 8080
+        self.ADDR = (self.IP, self.PORT)
+
+    def execute(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(self.ADDR)
+        server.listen()
+
+        print("[STARTING] Server has been started and now listening.")
+        print("[INFO] Server's addr is: " + str(self.IP) + ":"+ str(self.PORT))
+        print("[INFO] You can change ip and port by editing server.py file")
  
-    while True:
-        conn, addr = server.accept()
-        print(f"[NEW CONNECTION] {addr} connected.")
+        while True:
+            conn, addr = server.accept()
+            print(f"[NEW CONNECTION] {addr} connected.")
         
-        version = open(version.txt, "r")
-        conn.send(version.encode("utf-8"))
+            version = open(version.txt, "r")
+            conn.send(version.encode("utf-8"))
 
  
 
-        conn.send("File data received".encode("utf-8"))
+            conn.send("File data received".encode("utf-8"))
  
-        # завершаем
-        file.close()
-        conn.close()
-        print(f"[DISCONNECTED] {addr} disconnected.")
+            file.close()
+            conn.close()
+            print(f"[DISCONNECTED] {addr} disconnected.")
+
  
 if __name__ == "__main__":
-    main()
+    s = Server()
+    s.execute()
