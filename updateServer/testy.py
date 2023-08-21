839faf4e2e39881dcf1de71774d7de08cde7611c
A.Klivtsov <69029551+HappyAcidRain@users.noreply.github.com>
2023-08-21 15:38:28 +0300

    need to clear mainList from folders

--- updateServer/testy.py
+++ updateServer/testy.py
@@ -1,21 +1,31 @@
 import os 
 
-def pathCheck(dirList):
+def pathCheck(dirList, startPath):
 	for item in dirList:
 		if "." not in item:
-			newPath = os.getcwd() + '/' + item
+			newPath = startPath + '/' + item
 			newList = os.listdir(newPath)
 
 			for newItem in newList:
+				pathToCheck = []
+
 				if "." in newItem:
 					mainList.append(item + '/' + newItem)
 
 				else:
-					pathCheck(newList)
- 
-mainList = os.listdir()
+					pathToCheck.append(item + '/' + newItem)
 
-pathCheck(mainList)
+				if pathToCheck:
+					for path in  pathToCheck:
+						anoPath = startPath +'/' 
+						pathCheck(pathToCheck, anoPath)
+
+			
+
+path = 'C:/Users/sasha/OneDrive/Рабочий стол/gfhjg'
+
+mainList = os.listdir(path)
+pathCheck(mainList, path)
 		
-
 print(mainList)
+
