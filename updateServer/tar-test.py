18e42693060189cfdd1cf90b2571e1c78ff390f0
A.Klivtsov <69029551+HappyAcidRain@users.noreply.github.com>
2023-09-03 11:03:38 +0300

    tar-archivator is complete

--- updateServer/tar-test.py
+++ updateServer/tar-test.py
@@ -1,8 +1,23 @@
 import tarfile as tar
+import sqlite3
 
-archive = tar.open("ver1_3.tar", 'x:gz')
-archive.add('ver1_3/')
 
-# archive = tar.open("update", 'r:gz')
+folder = str(input("enter folder name: "))
+archiveName = folder + '.tar'
 
-# archive.extractall('exportFolder/')
+version = float(input("enter prog version (float): "))
+
+archive = tar.open(archiveName, 'x:gz')
+
+if '/' not in folder:
+	archive.add(folder + '/')
+else:
+	archive.add(folder)
+
+
+connect = sqlite3.connect("server.db")
+cursor = connect.cursor()
+cursor.execute("UPDATE server SET path = ?, version = ? WHERE ROWID = ?", (archiveName, version, 1))
+connect.commit()
+
+print('done!') 
